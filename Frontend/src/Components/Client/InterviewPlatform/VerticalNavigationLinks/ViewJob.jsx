6c4a3d6fbb109e2be39353483336e0a9bb61d43e
Roshan Prasad Sahu <roshanprasad447@gmail.com>
2025-03-22 20:46:27 +0530

    view job modified

--- Frontend/src/Components/Client/InterviewPlatform/VerticalNavigationLinks/ViewJob.jsx
+++ Frontend/src/Components/Client/InterviewPlatform/VerticalNavigationLinks/ViewJob.jsx
@@ -1,162 +1,71 @@
 import { useState } from 'react';
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
+
 function ViewJob() {
-     const [isModalOpen, setIsModalOpen] = useState(false); // Modal state
-      const navigate = useNavigate(); // Initialize useNavigate
-      const location = useLocation(); // Initialize useLocation
-      const rows = [
-        'SDE-II',
-        'SDE-III',
-        'SDET-II',
-        'SDET-I',
-        'EM',
-        'PE',
-        'DEVOPS-II',
-        'SDE-I',
-        'QA-I',
-        'QA-II',
-      ];
-      const jobData = [
-      { title: "Intro", candidates: "5 Min", guidelines: "a). xyzzzzzzzzzzzzzzzzzzzzz ", guidelines1: "b). suvvvvvvvvvvvvvvvvvvvvvvvv"   },
-      { title: "Java Concept", candidates: "25 Min", guidelines: "a). xyz ", guidelines1: "b). suv"  },
-      { title: "Database", candidates: "15 Min", guidelines: "a). xyz ", guidelines1: "b). suv"  },
-      { title: "Test Cases", candidates: "10 Min", guidelines: "a). xyz ", guidelines1: "b). suv"  },
-      { title: "FeedBack", candidates: "5 Min", guidelines: "a). xyz ", guidelines1: "b). suv"  },
+    
+        const [isVisible, setIsVisible] = useState(false);
+      
+        const handleToggle = () => {
+          setIsVisible(!isVisible);
+        };
      
-    ];
-    
-      const handleArchiveClick = () => {
-        setIsModalOpen(true);
-      };
-    
-      const closeModal = () => {
-        setIsModalOpen(false);
-      };
   return (
+
     <div className='w-full min-h-[calc(100vh-64px)] bg-[#EBDFD7] p-4 flex flex-col' >
-    <div className='flex flex-col gap-y-4 p-2 '>
-      <div className='' >
-        <div className="flex flex-col w-full justify-end sm:flex-row sm:items-center sm:space-x-4 space-y-4 sm:space-y-0 ml-auto">
-          {/* Search Input */}
-          <div className="flex items-center rounded-full px-4 py-2 w-[446px] bg-[#fff] border-gray-300 border focus-within:border-blue-700">
-            <input
-              type="text"
-              placeholder="Search job by Name, Email & Mobile Number"
-              className="flex-1 bg-transparent text-gray-600 outline-none text-sm"
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#000000"
-            >
-              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-            </svg>
-          </div>
-
-
-
-          {/* Add Client Button */}
-          <button
-            className="flex items-center justify-center space-x-2 bg-[#007AFF] text-white px-4 py-2 rounded-full text-sm font-medium w-full sm:w-auto shadow-[2px_4px_15px_rgba(5,109,220,0.5)]"
-            onClick={() => navigate(`${location.pathname}/addjob`, console.log("kshfgkjh")
-             )}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-            <span> Add Job</span>
-          </button>
-        </div>
+    <div className=" w-[70%] max-w-md mx-auto p-5 ">
+    
+    
+    <div className="mb-4 flex items-center justify-center gap-4 ">
+      <div className='w-[30%]' >
+      <label className="text-[15px] font-medium text-gray-700 whitespace-nowrap ">Job Role</label>
       </div>
-
-
-
-
-
-
-
-      <div className=' w-[100%] bg-[rgba(255,255,255,0.34)] p-2 mt-8 border rounded-lg ' > 
-        <div>
-            <div className='flex justify-between p-1' >
-                <div className='font-bold text-[20px] ' >Details</div>
-                <div className=' flex gap-x-3' >
-                    <button  >
-                      <div className='bg-white px-2 py-1 border rounded-full text-[15px] text-gray-500 ' >
-                      Job Description
-                      </div>
-                    </button>
-                    <button  >
-                      <div className='bg-white px-2 py-1 border rounded-full text-[15px] text-gray-500 ' >
-                      60 min
-                      </div>
-                    </button>
-                    <button  >
-                      <div className='bg-white px-2 py-1 border rounded-full text-[15px] text-gray-500 ' >
-                      sde-iii
-                      </div>
-                    </button>
-                </div>
-            </div>
-          <div>
-            <table className='w-[100%] h-[100%] ' >
-            <thead>
-              <tr className="border-b w-[100%]  ">
-                <th className="py-2 px-4 w-[10%] text-start font-medium text-black text-[18px] ">Users</th>
-                <th className="py-2 px-4 w-[30%] font-medium text-black whitespace-nowrap text-[18px]  ">Time</th>
-                <th className="py-2 px-4 w-[30%]  font-medium text-start text-black whitespace-nowrap pl-8 text-[18px] ">Guidelines</th>
-                <th className='py-2 px-4 w-[30%]  font-medium text-start text-black whitespace-nowrap pl-8' ></th>
-                <th className='py-2 px-4 w-[30%]  font-medium text-start text-black whitespace-nowrap pl-8' ></th>
-                
-                
-              </tr>
-            </thead>
-            <tbody>
-              {jobData.map((job, index) => (
-                <tr key={index} className="border-b w-[100%] p-2 ">
-                  <td className=" py-2 px-4 w-[10%] text-start ">{job.title}</td>
-                  <td className=" py-2 px-4 w-[30%] text-center  ">{job.candidates}</td>
-                  <td className=" py-2 px-4 w-[25%] text-start  ">
-                    <div>{job.guidelines}</div>
-                  </td>
-                  <td className='py-2 px-4 w-[25%] text-start   ' >
-                    <div>{job.guidelines1}</div>
-                  </td>
-                  <td className='py-2 px-4 w-[1%] text-start   ' >
-                    <svg fill="#000000" width="20px" height="20px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" id="Glyph" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M13,16c0,1.654,1.346,3,3,3s3-1.346,3-3s-1.346-3-3-3S13,14.346,13,16z" id="XMLID_294_"></path><path d="M13,26c0,1.654,1.346,3,3,3s3-1.346,3-3s-1.346-3-3-3S13,24.346,13,26z" id="XMLID_295_"></path><path d="M13,6c0,1.654,1.346,3,3,3s3-1.346,3-3s-1.346-3-3-3S13,4.346,13,6z" id="XMLID_297_"></path></g></svg>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-            </table>
-          </div>
-        </div>
-
+      <div className='w-[70%]' >
+      <input
+        type="text"
+        value="SDE III"
+        readOnly
+        className="w-full text-[15px] p-1 border border-gray-300 rounded-md"
+      />
       </div>
-
-
-
-
-     
-     
-
-
-
-
     </div>
+
+    <div className="mb-4 flex items-center justify-center gap-4">
+    <div className='w-[30%]' >
+        <label className="text-[15px] font-medium text-gray-700 whitespace-nowrap ">Interview Time</label>
+       </div>
+       <div className='w-[70%]' >
+      <input
+        type="text"
+        value="60 Minutes"
+        readOnly
+        className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+      />
+      </div>
     </div>
+
+    <div className="mb-4 flex items-center justify-center gap-4">
+    <div className='w-[30%]' >
+      <label className=" text-[15px] font-medium text-gray-700 whitespace-nowrap ">Job Description</label>
+      </div>
+      <div className='w-[70%]' >
+      <button  onClick={handleToggle}
+        className="w-full mt-1 p-2 bg-[rgb(255,255,255)] text-[#ff7043] font-medium rounded-full "
+      >
+        Click to View
+      </button>
+      </div>
+    </div>
+    {isVisible && (
+        <div className="max-w-md mx-auto p-5 border bg-[rgba(255,255,255,0.34)] border-gray-300 rounded-lg shadow mt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam porro at iste. Aliquid voluptatum assumenda veritatis,
+          similique quibusdam molestiae. Nesciunt quisquam facilis dolorum vitae quis at natus illo dicta neque.
+        </div>
+      )}
+
+    
+  </div>
+  </div>
   )
 }
 
