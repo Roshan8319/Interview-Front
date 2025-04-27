158812668eb0a6a984c8f82b604299094b418ba8
Dinanath Dash <dashdinanath056@gmail.com>
2025-04-27 21:22:54 +0530

    feat: update video URL in ViewDemo component and improve code formatting

--- Frontend/src/Components/Landing/ViewDemo.jsx
+++ Frontend/src/Components/Landing/ViewDemo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
 
-const VIDEO_URL = "https://www.youtube.com/embed/uIxOL9fKfEk";
+const VIDEO_URL = "https://app.supademo.com/embed/cm9zrmr0a0h8413m0o36aagxp?embed_v=2";
 
-const ViewDemo = ({ isOpen, onClose, videoUrl=VIDEO_URL }) => {
+const ViewDemo = ({ isOpen, onClose, videoUrl = VIDEO_URL }) => {
     return (
         <Dialog
             open={isOpen}
@@ -51,5 +51,19 @@ const ViewDemo = ({ isOpen, onClose, videoUrl=VIDEO_URL }) => {
     );
 };
 
-export {VIDEO_URL};
+export { VIDEO_URL };
 export default ViewDemo;
+
+
+{/* <div style="position: relative; box-sizing: content-box; max-height: 80vh; max-height: 80svh; width: 100%; aspect-ratio: 1.8252184769038702; padding: 40px 0 40px 0;">
+    <iframe src="https://app.supademo.com/embed/cm9zrmr0a0h8413m0o36aagxp?embed_v=2"
+        loading="lazy"
+        title="Recrumeta Demo"
+        allow="clipboard-write"
+        frameborder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowfullscreen
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+    </iframe>
+</div> */}
